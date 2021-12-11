e9cd4fb3e121bbd160038739acf61077e988b1c5
Liam Ok <liamokane19@gmail.com>
2021-12-11 22:01:22 +1100

    rolled out the array for lowercase letters aswell as numbers from 0-10. console.log check on the browser and no errors

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ var specialSybmbols = ["!", "@", "#", "$", "%", "^", "&", "*", ")", "("];
 
 console.log(specialSybmbols);
 
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r","s","t","u","v","w","x","y","z"];
+
+console.log(lowercaseLetters);
+
+var numbers = 12345678910;
+
+console.log(numbers);
+
