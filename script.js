24f461b1f38dbb778097223f0c0e7fb1369ea22d
Liam Ok <liamokane19@gmail.com>
2021-12-12 17:33:18 +1100

    commented out the alert box. I worked on the generate password function first. Which is now running, now to fill in the middle section for the user to determine lower-uppercase/special characters and numbers parameters

--- script.js
+++ script.js
@@ -13,27 +13,32 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var chars = "12345678910abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var passwordLength = (8 > 128);
+var chars = "12345678910abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = (8 );
 var password = "";
 
+for (var i = 0; i <=passwordLength; i++){
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
 
-var specialSybmbols = ["!", "@", "#", "$", "%", "^", "&", "*", ")", "("];
+}
 
-console.log(specialSybmbols);
+document.getElementById("password").value = password;
 
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r","s","t","u","v","w","x","y","z"];
 
-console.log(lowercaseLetters);
 
-var numbers = 12345678910;
+// the below is commented out. I started with this process. I would like to know whether I can still use this in the future?
+// var specialSybmbols = ["!", "@", "#", "$", "%", "^", "&", "*", ")", "("];
+// console.log(specialSybmbols);
+// var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r","s","t","u","v","w","x","y","z"];
+// console.log(lowercaseLetters);
+// var numbers = 12345678910;
+// console.log(numbers);
 
-console.log(numbers);
+// var userChoice = window.prompt("Your Password must be between 8-128 characters long. Would you like to include Upper Case, type Y for yes or N for no");
 
-var userChoice = window.prompt("Your Password must be between 8-128 characters long. Would you like to include Upper Case, type Y for yes or N for no");
+// var passwordLength = "";
+//   for ( let i = 0; i < lowercaseLetters[0].length; i++);
 
-var passwordLength = "";
-  for ( let i = 0; i < lowercaseLetters[0].length; i++);
-
-  console.log (passwordLength);
+//   console.log (passwordLength);
 
