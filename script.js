68aad4149ea8640599fc76724ae5e27cd20b9930
Liam Ok <liamokane19@gmail.com>
2021-12-13 11:45:26 +1100

    click btn function wasn't working because of the closed parenthesis at the bottom of the function wasn't added. The boolean true functions now running. Need to research why the false isn't

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 //seperate variable strings. 
-var numbers = ["0","1","2","3","4","5","6","7","8","9","10"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var specialSybmbols = ["!", "@", "#", "$", "%", "^", "&", "*", ")", "("];
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r","s","t","u","v","w","x","y","z"];
 var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -11,7 +11,8 @@ var chosenPassword = [];
 
 function generatePassword () {
 
-var passwordLength = window.prompt ("Let's begin with you choosen a length for your password it must be between 8-128 characters long!" );
+var passwordLength = window.prompt ("Let's begin with you choosen a length for your password it must be between 8-128 characters long");
+
 
 if (isNaN(passwordLength)) {
   window.alert ("Please chose a number!");
@@ -23,18 +24,20 @@ if (passwordLength < 8 || passwordLength > 128) {
   return generatePassword();
 }
 
-var requestNumbers = window.confirm ("Click 'Ok' to include Numbers, otherwise hit Cancel !")
-var requestSymbols = window.confirm ("Click 'Ok' to include Symbols, otherwise hit Cancel !")
-var requestLowercase = window.confirm ("Click 'Ok' to include Lowercase, otherwise hit Cancel !")
-var requestUppercase = window.confirm ("Click 'Ok' to include Uppercase, otherwise hit Cancel !")
+var requestNumbers = window.confirm ("Click 'Ok' to include Numbers, otherwise hit Cancel !");
+var requestSymbols = window.confirm ("Click 'Ok' to include Symbols, otherwise hit Cancel !");
+var requestLowercase = window.confirm ("Click 'Ok' to include Lowercase, otherwise hit Cancel !");
+var requestUppercase = window.confirm ("Click 'Ok' to include Uppercase, otherwise hit Cancel !");
 
 if (!requestNumbers && !requestSymbols && !requestLowercase && !requestUppercase) {
-  window.alert ("In the following windows, please select a character for each !")
+  window.alert ("In the following windows, please select a character for each !");
   return generatePassword();
 }
 
 if (requestNumbers = true){
   chosenPassword.push(...numbers);
+} else (requestNumbers = false){
+  chosenPassword.pop (...numbers);
 }
 
 if (requestSymbols = true){
@@ -56,7 +59,7 @@ for (i = 0; i<passwordLength; i++){
   result += chosenPassword[index];
 }
 
-return result;
+return result;}
 
 
 // Write password to the #password input
@@ -84,4 +87,4 @@ document.getElementById("password").value = password;
 // } 
 // else (userChoice === noUppercase); {
 //   window.alert("You chose no to Uppercase. Click ok for the next step ");
-// }
+
